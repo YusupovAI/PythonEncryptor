d4db82222bd899ec2b566f144cf0fda1a33838a5
YusupovAI <iusupov.ai@phystech.edu>
2019-04-28 00:40:56 +0300

    Added stress testing

---
+++ tests/stress_tests/test_encode_decode.py
@@ -0,0 +1,33 @@
+from app import decode, encode
+import string
+import random
+
+
+def generate_text():
+    return ''.join(random.choices(
+        string.ascii_uppercase + string.digits + string.ascii_lowercase +
+        string.punctuation + string.punctuation,
+        k=random.randint(0, 10000)))
+
+
+def test_caesar():
+    for i in range(100):
+        text = generate_text()
+        key = random.randint(0, 10000)
+        assert decode.caesar(encode.caesar(text, key), key) == text
+
+
+def test_vigenere():
+    for i in range(100):
+        text = generate_text()
+        key = ''.join(
+            random.choices(string.ascii_lowercase + string.ascii_uppercase,
+                           k=random.randint(0, 10000)))
+        assert decode.vigenere(encode.vigenere(text, key), key) == text
+
+
+def test_vernam():
+    for i in range(100):
+        text = generate_text()
+        key = ''.join(random.choices(['0', '1'], k=random.randint(0, 10000)))
+        assert decode.vernam(encode.vernam(text, key), key) == text
