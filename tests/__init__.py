bf828c5816671b9514054d63306ec50e5a8ce42e
YusupovAI <iusupov.ai@phystech.edu>
2019-05-05 13:55:32 +0300

    Fixed travis

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-graphics_enabled = True
+graphics_enabled = False
 sizes = [100, 1000, 5000, 10000, 50000, 10 ** 5, 5 * (10 ** 5),
          10 ** 6, 2 * 10 ** 6, 35 * 10 ** 5, 5 * 10 ** 6]
 texts_enabled = False
